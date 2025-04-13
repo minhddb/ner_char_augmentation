ad1fb6b1fd6271f236680a5df39b89c49bbc4b0a
Minh Doan <minh.ddb@gmail.com>
2025-04-13 23:07:34 +0200

    revised run_ner.py for incremental augmentation learning

--- run_ner.py
+++ run_ner.py
@@ -8,9 +8,11 @@ from typing import Dict
 from dataset import WikiANNDataLoader, GermEval14, GermanLER, WNUTDataLoader
 from augmentation import Augmentation
 
-import torch
 import evaluate
 import numpy as np
+
+import torch
+from torch.utils.data import DataLoader, SequentialSampler
 
 from seqeval.scheme import IOB2
 from seqeval.metrics import precision_score, recall_score, f1_score
@@ -18,7 +20,8 @@ from seqeval.metrics import precision_score, recall_score, f1_score
 import datasets
 from datasets import Dataset, concatenate_datasets
 
-import transformers
+from transformers.utils import is_datasets_available
+from transformers.trainer_utils import seed_worker
 from transformers import (AutoTokenizer, AutoModelForTokenClassification, AutoConfig,
                           TrainingArguments, Trainer, DataCollatorForTokenClassification)
 
@@ -96,56 +99,44 @@ class Tokenization:
     @staticmethod
     def align_labels_to_ids(label, label2id):
         return label2id[label]
+    
+class TrainerForTextAugmentation(Trainer):
+    def get_train_dataloader(self) -> DataLoader:
+        """
+        Returns the training [`~torch.utils.data.DataLoader`].
+
+        Use SequentialSampler instead of random sampler to keep the order of training data 
+        """
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        train_dataset = self.train_dataset
+        data_collator = self.data_collator
+        if is_datasets_available() and isinstance(train_dataset, datasets.Dataset):
+            train_dataset = self._remove_unused_columns(train_dataset, description="training")
+        else:
+            data_collator = self._get_collator_with_removed_columns(data_collator, description="training")
+
+        dataloader_params = {
+            "batch_size": self._train_batch_size,
+            "collate_fn": data_collator,
+            "num_workers": self.args.dataloader_num_workers,
+            "pin_memory": self.args.dataloader_pin_memory,
+            "persistent_workers": self.args.dataloader_persistent_workers,
+        }
+
+        if not isinstance(train_dataset, torch.utils.data.IterableDataset):
+            dataloader_params["sampler"] = SequentialSampler(train_dataset)
+            dataloader_params["drop_last"] = self.args.dataloader_drop_last
+            dataloader_params["worker_init_fn"] = seed_worker
+            dataloader_params["prefetch_factor"] = self.args.dataloader_prefetch_factor
+
+        return self.accelerator.prepare(DataLoader(train_dataset, **dataloader_params))    
 
 def model_for_token_classification(model_name, config):
     def model_init():
         return AutoModelForTokenClassification.from_pretrained(model_name, config=config)
     return model_init
-
-class AutoTokenClassifier:
-    def __init__(self, trainer: transformers.Trainer, tokenized_dataset: datasets.Dataset, id_to_label_map: Dict):
-        self.trainer = trainer
-        self.tokenized_dataset = tokenized_dataset
-        self.id_to_label_map = id_to_label_map
-        self.predictions, self.gold = self._predict()
-
-    def predict(self):
-        # predictions, gold = self._predict()
-        result_metrics = self.compute_metrics(self.predictions, self.gold)
-        print(result_metrics)
-        return result_metrics
-
-    def _predict(self):
-        predictions, gold, _ = self.trainer.predict(self.tokenized_dataset)
-        if isinstance(predictions, torch.Tensor):
-            predictions = predictions.detach().cpu().numpy()
-        if isinstance(gold, torch.Tensor):
-            gold = gold.detach().cpu().numpy()
-        
-        predictions = np.argmax(predictions, axis=2)
-        return predictions, gold
-
-    def compute_metrics(self, predictions: torch.Tensor, gold: torch.Tensor):
-        evaluations = dict(precision=None,
-                           recall=None,
-                           f1=None
-                           )
-        label_list = list(self.id_to_label_map.values())
-        true_predictions = [
-            [label_list[p] for (p, l) in zip(pred, label) if l != -100]
-            for pred, label in zip(predictions, gold)
-        ]
-
-        true_labels = [
-            [label_list[l] for (p, l) in zip(pred, label) if l != -100]
-            for pred, label in zip(predictions, gold)
-        ]
-
-        # Metrics
-        evaluations["precision"] = precision_score(true_labels, true_predictions, mode="strict", scheme=IOB2)
-        evaluations["recall"] = recall_score(true_labels, true_predictions, mode="strict", scheme=IOB2)
-        evaluations["f1"] = f1_score(true_labels, true_predictions, mode="strict", scheme=IOB2)
-        return evaluations
 
 def compute_metrics(p):
     metrics = evaluate.load("seqeval")
@@ -160,18 +151,20 @@ def compute_metrics(p):
         [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
-    results = metrics.compute(predictions=true_predictions, references=true_labels, zero_division=0)
-    return {"eval_precision": results["overall_precision"],
-            "eval_recall": results["overall_recall"],
-            "eval_f1": results["overall_f1"],
+    results = metrics.compute(predictions=true_predictions, references=true_labels, zero_division=0, mode="strict", scheme="IOB2")
+    return {"precision": results["overall_precision"],
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
             }
 
-def generate_data(data, tags_column, ratios, stratified_samples=True):
+def generate_data(data: Dataset, tags_column, ratios, stratified_samples=True, seed=0):
+    if "id" in data.features.keys():
+        data = data.remove_columns("id")
     for ratio in ratios:
         if ratio == 0:
-            yield ratio, "no_augmentation", data
+            yield ratio, "no_augmentation", data.shuffle(seed=seed)
         else:
-            augmentation = Augmentation(raw_train, id2label, tag_column=tags_column, stratified_samples=stratified_samples, ratio=ratio, iteration=1)
+            augmentation = Augmentation(data, id2label, tag_column=tags_column, stratified_samples=stratified_samples, ratio=ratio, iteration=1)
             augmentation_samples = augmentation()
             for aug in ["reverse_letter_case", "delete_character", "swap_characters", "substitute_character", "all"]:
                 if aug != "all":
@@ -180,20 +173,22 @@ def generate_data(data, tags_column, ratios, stratified_samples=True):
                     augmented = [sample for key in augmentation_samples.keys() for sample in augmentation_samples[key]]
                 augmented_dataset = Dataset.from_list(augmented) 
                 # Insert noise injected instances to existing dataset with concatenation
-                if "id" not in raw_train.features.keys():
+                if "id" not in data.features.keys():
                     augmented_dataset = augmented_dataset.remove_columns("id")
-                casted_augmented_dataset = augmented_dataset.cast(raw_train.features)
-                yield ratio, aug, concatenate_datasets([data, casted_augmented_dataset ])
+                casted_augmented_dataset = augmented_dataset.cast(data.features)
+                # Shuffle the original and augmented datasets seperately before concatenating them together
+                yield ratio, aug, concatenate_datasets([data.shuffle(seed=seed), casted_augmented_dataset.shuffle(seed=seed)])
             
 if __name__=="__main__":
     args = arguments()
-    args.hf_dataset = "wikiann_de"
-    args.model_name = "TUM/GottBERT_base_last"
-    args.stratified_samples = True
-    args.do_test = True
 
     TEXT_COLUMN = "tokens"
     TAGS_COLUMN = "ner_tags"
+
+    if args.hf_dataset == "wikiann_en":
+        raw_dataset, label2id, id2label = WikiANNDataLoader().__call__(language="en")
+        label_list = list(label2id.keys())
+        TAGS_COLUMN = "tags"
 
     if args.hf_dataset == "wikiann_de":
         raw_dataset, label2id, id2label = WikiANNDataLoader().__call__(language="de")
@@ -230,11 +225,10 @@ if __name__=="__main__":
     with open(path_results, "w", encoding="utf-8") as out_f:
         out_f.write("model_name\tseed\taug_strategy\tratio\titeration\ttrain_size\tprecision\trecall\tf1\n")
     
-    sample_ratios = [0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+    sample_ratios = [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1]
     # ratios = [0]
     for ratio, aug, train_dataset in generate_data(raw_train, TAGS_COLUMN, sample_ratios, args.stratified_samples):
-        for run in range(1):
-            # set_seed(run)
+        for run in range(40,45):
             config = Configuration(args.model_name,
                                    num_labels=num_labels,
                                    label_to_id=label2id,
@@ -269,6 +263,7 @@ if __name__=="__main__":
                                               num_train_epochs=5,
                                               do_train=True,
                                               do_eval=True,
+                                              do_predict=args.do_test,
                                               gradient_accumulation_steps=4,
                                               eval_strategy="epoch",
                                               logging_strategy="epoch",
@@ -280,7 +275,7 @@ if __name__=="__main__":
                                               )
 
             # Trainer
-            trainer = Trainer(# model=model,
+            trainer = TrainerForTextAugmentation(# model=model,
                               args=training_args,
                               train_dataset=tokenized_train_dataset,
                               eval_dataset=tokenized_val_dataset,
@@ -301,12 +296,12 @@ if __name__=="__main__":
             trainer.save_metrics("eval", eval_metrics)
 
             if args.do_test:
-                output_preds = []
-                classifier = AutoTokenClassifier(trainer, tokenized_test_dataset, id2label)
-                test_results = classifier.predict()
-
+                logits, labels, preds_results = trainer.predict(tokenized_test_dataset)
+                print(preds_results)
+                
+                # output_preds = []
+# 
                 # Write results to tsv file for further analysis
                 with open(path_results, "a", encoding="utf-8") as out_f:
-                    out_f.write(f"{args.model_name}\t{run}\t{aug}\t{ratio}\t{1}\t{len(train_dataset["tokens"])}\t{test_results['precision']}\t{test_results['recall']}\t{test_results['f1']}\n")
-                print(f"Run: {aug} {ratio} {1} {run}\n")
-                print(f"P: {test_results['precision']} R: {test_results['recall']} F1: {test_results['f1']}")
+                    out_f.write(f"{args.model_name}\t{run}\t{aug}\t{ratio}\t{1}\t{len(train_dataset["tokens"])}\t{preds_results['test_precision']}\t{preds_results['test_recall']}\t{preds_results['test_f1']}\n")
+                
